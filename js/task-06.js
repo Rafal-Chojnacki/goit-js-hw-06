0a4bd9a09f4de55c217c9b06e0ce776cffb5144d
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-04-12 20:15:31 +0200

    Task-06 new version

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,3 @@
 const input = document.querySelector('#validation-input');
-input.addEventListener('blur', function() {
-    const inputValueLength = input.value.length;
-    
-    if (inputValueLength >= 6) {
-      input.style.borderColor = '#4CAF50';
-    } else {
-      input.style.borderColor = '#F44336';
-    }
-});
+input.addEventListener('blur', () => input.value.length >= 6 ? input.style.borderColor = '#4CAF50' : input.style.borderColor = '#F44336'
+);
