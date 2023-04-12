a487964b0e89c0cdf49f3512e8af41d96ca768b0
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-04-12 21:39:14 +0200

    Task-07

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const fontSizeControl = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+const textString = text.textContent;
+fontSizeControl.addEventListener('input', () => {
+  const fontSize = fontSizeControl.value + 'px';
+  text.style.fontSize = fontSize;
+});
+ 
